77adc4e288250202a84479e366b1ee36b805933c
Alexander Kiryukhin <ormakent@gmail.com>
2019-02-10 13:33:15 -0800

    - added screen capture

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 GAME = {
-    'resolution': '640x480',
+    'width': 640,
+    'height': 480,
     'path': '/usr/local/bin'
+}
+
+SYSTEM = {
+    'width': 2880,
+    'height': 1800
 }
--- launcher.py
+++ launcher.py
@@ -3,16 +3,22 @@ import pyautogui
 import time
 import logging
 import config
+import PIL
 
 log = logging.getLogger('launcher')
+game_running = False
 
 
 def launch_game():
-    process_options = ["mame", "amspdwy", "-resolution", config.GAME['resolution'], "-window", "-sound", "none"]
-    subprocess.Popen(process_options, cwd=config.GAME['path'])
+    if game_running:
+        restart_game()
+    else:
+        resolution = str(config.GAME['width']) + "x" + str(config.GAME['height'])
+        process_options = ["mame", "amspdwy", "-resolution", resolution, "-window", "-sound", "none"]
+        subprocess.Popen(process_options, cwd=config.GAME['path'])
+        # wait for game to start up
+        time.sleep(3)
 
-    # wait for game to start up
-    time.sleep(3)
 
     # skip machine info window
     for i in range(5):
@@ -20,19 +26,38 @@ def launch_game():
         time.sleep(0.1)
 
     # wait for screen transition
-    time.sleep(3)
+    time.sleep(4)
 
-    '''
-    # insert coin
+
+def restart_game():
+    pyautogui.press("f3")
+
+
+def exit_game():
+    pyautogui.press("esc")
+
+
+def insert_coin():
     pyautogui.press("5")
-    # wait for race to start
-    time.sleep(8)
-    # drive forward
-    pyautogui.keyDown("UP")
-    time.sleep(20)
-    pyautogui.keyUp("UP")
-    '''
+
+
+def take_screenshot():
+    start = time.time()
+    # pretty slow screenshot taker, may need to replace
+    img = pyautogui.screenshot(region=(config.SYSTEM['width'] / 2 - config.GAME['width'],
+                                       config.SYSTEM['height'] / 2 - config.GAME['height'],
+                                       config.GAME['width'] * 2,
+                                       config.GAME['height'] * 2))
+    img.thumbnail((config.GAME['width'], config.GAME['height']), PIL.Image.ANTIALIAS)
+    img.save("test.png")
+    print(f'it took {time.time() - start} seconds to process a screenshot')
+
+
+def main():
+    launch_game()
+    take_screenshot()
+    exit_game()
 
 
 if __name__ == "__main__":
-    launch_game()
+    main()
